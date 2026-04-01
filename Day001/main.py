87844c67918d6246d585c2b89ead6300585ddc5d
chaitanya <chaysinfo@gmail.com>
2026-04-01 06:35:15 +0530

    Create main.py

---
+++ Day001/main.py
@@ -0,0 +1,4 @@
+print("Welcome to the Brand Name Generator.")
+city = input("Which city did you grow up in? ")
+pet = input("What's your pet's name? ")
+print("Your band name can be: " + city + " " + pet)
