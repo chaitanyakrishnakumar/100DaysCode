aadd82a94c4b6f90e67fce30077bbb0e790fc203
chaitanya <chaysinfo@gmail.com>
2026-04-13 11:27:37 +0530

    Create main.py

---
+++ Day013/Exercise 2 - Debugging Leap Year/main.py
@@ -0,0 +1,15 @@
+def is_leap(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
+
+# bug is in line 4
+# bug: '4000'
+# fix: '400'
